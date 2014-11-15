8fe47edfb46f31c2df72a1f7d235561296fd33a2
oropivan <orop.ivan@gmail.com>
2014-11-15 15:16:20 -0600

    updating filter

--- src/filter.py
+++ src/filter.py
@@ -8,125 +8,137 @@ from os import listdir
 from os.path import isfile, join
 import sys
 import numpy as np
-from hmmUsage import transition_probabilities
 
 # POS_TAGS = ["CC", "CD", "DT", "EX", "FW", "IN", "JJ", "JJR", "JJS", "LS", "MD", "NN", "NNS", "NNP", "NNPS", "PDT", "POS", "PRP", "PRP$", "RB", "RBR", "RBS", "RP", "SYM", "TO", "UH", "VB", "VBD", "VBG", "VBN", "VBP", "VBZ", "WDT", "WP", "WP$", "WRB"]
-SYMBOLS_SEEN = set()
-POS_TAGS_SEEN = set()
-POS_TAGS_SEEN.add("<S>")
-POS_TAGS_SEEN.add("<\S>")
 
-map_wordPOS_count = {}
-map_POSPOS_count = {}
-
-map_POS_count = {}
-map_word_count = {}
-
-map_symbol_index = {}
-map_POS_index = {}
-
-sentences = []
 def readFile(systemRun, numberOfSentencesToTrain, currentCount):
+    sentences = []
     previousTag = None
     sentence = []
-    with open(systemRun, 'rb') as file:
-        for line in file:
+    with open(systemRun, 'rb') as datafile:
+        for line in datafile:
             if not line.strip(): 
                 continue
             if line.startswith("==="):
                 if previousTag is None:
-                    previousTag = "<S>"
-                    map_POS_count[previousTag] = map_POS_count.get(previousTag, 0) + 1
                     sentence = []
                     continue
-                tag = "<\S>"
-                map_POS_count[tag] = map_POS_count.get(tag, 0) + 1
-                key = tuple([previousTag,tag])
-                map_POSPOS_count[key] = map_POSPOS_count.get(key, 0) + 1
                 if currentCount == numberOfSentencesToTrain:
                     return numberOfSentencesToTrain
                 else: 
                     sentences.append(sentence)
-                    sentence = []
-                    previousTag = "<S>"
-                    map_POS_count[previousTag] = map_POS_count.get(previousTag, 0) + 1
                     currentCount += 1
+                    sentence = []
                     continue
             if line.startswith("["):
                 line = line[1:-2].strip()
             line = line.split()
             for observation in line:
-                word, tag = observation.split('/')
+                wordTagTuple = observation.split('/')
+                if len(wordTagTuple) == 2:
+                    word, tag = wordTagTuple
+                else: # for words that have /
+                    tag = wordTagTuple[-1]
+                    word = '/'.join(wordTagTuple[:-1])
                 word = word.upper()
                 sentence.append((word, tag))
-                
-                # add to vocabulary
-                POS_TAGS_SEEN.add(tag)
-                SYMBOLS_SEEN.add(word)
-                
-                # count word,TAG instances
-                key = tuple([word,tag])
-                map_wordPOS_count[key] = map_wordPOS_count.get(key, 0) + 1
-                
-                #count tag tag instances
-                key = tuple([previousTag,tag])
-                map_POSPOS_count[key] = map_POSPOS_count.get(key, 0) + 1
-                
-                # count tag and word instances
-                map_word_count[word] = map_word_count.get(word, 0) + 1
-                map_POS_count[tag] = map_POS_count.get(tag, 0) + 1
-                
-                previousTag = tag
-    tag = "<\S>"
-    map_POS_count[tag] = map_POS_count.get(tag, 0) + 1
-    key = tuple([previousTag,tag])
-    map_POSPOS_count[key] = map_POSPOS_count.get(key, 0) + 1
     sentences.append(sentence)
-    return (currentCount, sentences)
+    currentCount += 1
+    return sentences
 
-def dirTraverse(path, numberOfSentencesToTrain, currentCount):
+def getCountsFromSentences(sentences):
+    SYMBOLS_SEEN = set()
+    POS_TAGS_SEEN = set()
+    
+    map_wordPOS_count = {}
+    map_POSPOS_count = {}
+    
+    map_POS_count = {}
+    map_word_count = {}
+    for sentence in sentences:
+        previousTag = '<S>'
+        POS_TAGS_SEEN.add(previousTag)
+        map_POS_count[previousTag] = map_POS_count.get(previousTag, 0) + 1
+        for word, tag in sentence:
+            # add to vocabulary
+            POS_TAGS_SEEN.add(tag)
+            SYMBOLS_SEEN.add(word)
+            
+            # count tag and word instances
+            map_word_count[word] = map_word_count.get(word, 0) + 1
+            map_POS_count[tag] = map_POS_count.get(tag, 0) + 1
+            
+            # count word,TAG instances
+            key = tuple([word,tag])
+            map_wordPOS_count[key] = map_wordPOS_count.get(key, 0) + 1
+             
+            #count tag tag instances
+            key = tuple([tag,previousTag])
+            map_POSPOS_count[key] = map_POSPOS_count.get(key, 0) + 1
+             
+            previousTag = tag
+        tag = "<\S>"
+        POS_TAGS_SEEN.add(tag)
+        map_POS_count[tag] = map_POS_count.get(tag, 0) + 1
+        
+        key = tuple([tag,previousTag])
+        map_POSPOS_count[key] = map_POSPOS_count.get(key, 0) + 1
+        
+    return (SYMBOLS_SEEN, POS_TAGS_SEEN, map_wordPOS_count, map_POSPOS_count, map_POS_count, map_word_count)
+        
+def dirTraverse(path, numberOfSentencesToTrain, currentCount, sentences):
     files = [ f for f in listdir(path)]
     for fileName in files:
         if currentCount == numberOfSentencesToTrain:
             break
         fileName = "/".join([path,fileName])
         if isfile(fileName):
-            currentCount = readFile(fileName, numberOfSentencesToTrain, currentCount)
+            sentencesRead = readFile(fileName, numberOfSentencesToTrain, currentCount)
+            currentCount += len(sentencesRead)
+            sentences = sentences + sentencesRead
         else:
-            currentCount = dirTraverse(fileName, numberOfSentencesToTrain, currentCount)
-    return currentCount
-def createEmissionProbabilities(map_symbol_index, map_POS_index):
-    emissionProbs = np.zeros((len(SYMBOLS_SEEN), len(POS_TAGS_SEEN)))
-    for POS in POS_TAGS_SEEN:
-        for word in SYMBOLS_SEEN:
+            currentCount, sentences = dirTraverse(fileName, numberOfSentencesToTrain, currentCount, sentences)
+    return (currentCount, sentences)
+
+def createEmissionProbabilities(symbolsSeen, POS_tagsSeen, map_symbol_index, map_POS_index, map_wordPOS_count, map_word_count):
+    emissionProbs = np.zeros((len(symbolsSeen), len(POS_tagsSeen)))
+    for POS in POS_tagsSeen:
+        for word in symbolsSeen:
             key = tuple([word, POS])
             numerator = float(map_wordPOS_count.get(key,0))
             denominator = float(map_word_count[word])
             emissionProbs[map_symbol_index[word]][map_POS_index[POS]] = numerator/denominator
     return emissionProbs
 
-def createTransitionProbabilities(map_POS_index):
-    transitionProb = np.zeros((len(POS_TAGS_SEEN), len(POS_TAGS_SEEN)))
-    for pPOS in POS_TAGS_SEEN:
-        for cPOS in POS_TAGS_SEEN:
-            key = tuple([pPOS,cPOS])
+def createTransitionProbabilities(POS_tagsSeen, map_POS_index, map_POSPOS_count, map_POS_count):
+    transitionProb = np.zeros((len(POS_tagsSeen), len(POS_tagsSeen)))
+    for pPOS in POS_tagsSeen:
+        for cPOS in POS_tagsSeen:
+            key = tuple([cPOS,pPOS])
             numerator = float(map_POSPOS_count.get(key,0))
             denominator = float(map_POS_count[pPOS])
             transitionProb[map_POS_index[pPOS]][map_POS_index[cPOS]] = numerator/denominator
     return transitionProb            
                      
 def createConditionalProbabilitiesTables(rootPath, numberOfSentencesToTrain):
-    dirTraverse(rootPath, numberOfSentencesToTrain, 0)
-    map_symbol_index = {v: k for k, v in dict(enumerate(SYMBOLS_SEEN)).items()}
-    map_POS_index = {v: k for k, v in dict(enumerate(POS_TAGS_SEEN)).items()}
+    sentenceCount, sentences = dirTraverse(rootPath, numberOfSentencesToTrain, 0, [])
+    symbolsSeen, POS_tagsSeen, map_wordPOS_count, map_POSPOS_count, map_POS_count, map_word_count = getCountsFromSentences(sentences)
+    map_symbol_index = {v: k for k, v in dict(enumerate(symbolsSeen)).items()}
+    map_POS_index = {v: k for k, v in dict(enumerate(POS_tagsSeen)).items()}
     
-    emission_probabilities = createEmissionProbabilities(map_symbol_index, map_POS_index)
-    transition_probabilities = createTransitionProbabilities(map_POS_index)
-#     print map_POS_index
-#     print [sum(transition_probabilities[i]) for i in range(transition_probabilities.shape[0])]
-#     
-#     print map_symbol_index
-#     print [sum(emission_probabilities[i]) for i in range(emission_probabilities.shape[0])]
-    return (transition_probabilities, emission_probabilities)
+    emission_probabilities = createEmissionProbabilities(symbolsSeen, POS_tagsSeen, map_symbol_index, map_POS_index, map_wordPOS_count, map_word_count)
+    transition_probabilities = createTransitionProbabilities(POS_tagsSeen, map_POS_index, map_POSPOS_count, map_POS_count)
+    print map_POS_index
+    print transition_probabilities
+    print [sum(transition_probabilities[i]) for i in range(transition_probabilities.shape[0])]
+     
+    print map_symbol_index
+    print [sum(emission_probabilities[i]) for i in range(emission_probabilities.shape[0])]
+    return (map_symbol_index, map_POS_index, transition_probabilities, emission_probabilities)
+
+def main():
+    path = "/home/czar/dev/GraphModels/finalProject/data/pos/wsj"
+    number = 1
+    createConditionalProbabilitiesTables(path, number)
 if __name__ == "__main__":
     main()
